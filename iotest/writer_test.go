4ee2022941cfd432038e053e7fa7b4df284400a7
Red Forks <redforks@gmail.com>
2019-12-06 16:12:06 +0800

    Rewrite iotest using testify

--- iotest/writer_test.go
+++ iotest/writer_test.go
@@ -3,68 +3,56 @@ package iotest_test
 import (
 	"bytes"
 	"io"
+	"testing"
 	"time"
 
-	"github.com/redforks/testing/reset"
-
 	. "github.com/redforks/testing/iotest"
-
-	bdd "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
 
-func validErrorWrite(w io.Writer, dataLen, writes int) {
+func validErrorWrite(t *testing.T, w io.Writer, dataLen, writes int) {
 	n, err := w.Write(make([]byte, dataLen))
-	Ω(err).Should(MatchError(ErrWriter))
-	Ω(n).Should(Equal(writes), "ErrorWriter should write %d bytes", writes)
+	assert.Equal(t, ErrWriter, err)
+	assert.Equal(t, writes, n)
 }
 
-func validErrorWriteSuccess(w io.Writer, dataLen int) {
-	Ω(w.Write(make([]byte, dataLen))).Should(Equal(dataLen), "ErrorWriter should write succeed")
+func validErrorWriteSuccess(t *testing.T, w io.Writer, dataLen int) {
+	buf := make([]byte, dataLen)
+	assertWrite(t, w, buf)
 }
 
-var _ = bdd.Describe("Writers", func() {
+func TestErrorWriter(t *testing.T) {
+	w := ErrorWriter(0)
+	validErrorWrite(t, w, 10, 0)
 
-	bdd.BeforeEach(func() {
-		reset.Enable()
-	})
+	w = ErrorWriter(5)
+	validErrorWriteSuccess(t, w, 3)
+	validErrorWrite(t, w, 10, 2)
+	validErrorWrite(t, w, 10, 0)
 
-	bdd.AfterEach(func() {
-		reset.Disable()
-	})
+	w = ErrorWriter(5)
+	validErrorWriteSuccess(t, w, 5)
+	validErrorWrite(t, w, 10, 0)
+}
 
-	bdd.It("ErrorWriter", func() {
-		w := ErrorWriter(0)
-		validErrorWrite(w, 10, 0)
+func assertWrite(t *testing.T, w io.Writer, buf []byte) {
+	n, err := w.Write(buf)
+	assert.NoError(t, err)
+	assert.Equal(t, len(buf), n)
+}
 
-		w = ErrorWriter(5)
-		validErrorWriteSuccess(w, 3)
-		validErrorWrite(w, 10, 2)
-		validErrorWrite(w, 10, 0)
+func TestSlowWriter(t *testing.T) {
+	buf := &bytes.Buffer{}
+	w := NewSlowWriter(buf, 15*time.Millisecond)
+	tStart := time.Now()
+	assertWrite(t, w, []byte("abc"))
+	tEnd := time.Now()
+	assert.Equal(t, []byte("abc"), buf.Bytes())
+	assert.WithinDuration(t, tStart.Add(15*time.Millisecond), tEnd, 10*time.Millisecond)
 
-		w = ErrorWriter(5)
-		validErrorWriteSuccess(w, 5)
-		validErrorWrite(w, 10, 0)
-	})
-
-	bdd.It("SlowWriter", func() {
-		buf := &bytes.Buffer{}
-		w := NewSlowWriter(buf, 15*time.Millisecond)
-		tStart := time.Now()
-		Ω(w.Write([]byte("abc"))).Should(Equal(3))
-		tEnd := time.Now()
-		d := tEnd.Sub(tStart)
-		Ω(buf.Bytes()).Should(Equal([]byte("abc")))
-		Ω(d > 15*time.Millisecond).Should(BeTrue())
-		Ω(d < 25*time.Millisecond).Should(BeTrue())
-
-		tStart = time.Now()
-		Ω(w.Write([]byte("cde"))).Should(Equal(3))
-		tEnd = time.Now()
-		Ω(buf.Bytes()).Should(Equal([]byte("abccde")))
-		d = tEnd.Sub(tStart)
-		Ω(d > 15*time.Millisecond).Should(BeTrue())
-		Ω(d < 25*time.Millisecond).Should(BeTrue())
-	})
-
-})
+	tStart = time.Now()
+	assertWrite(t, w, []byte("cde"))
+	tEnd = time.Now()
+	assert.Equal(t, []byte("abccde"), buf.Bytes())
+	assert.WithinDuration(t, tStart.Add(15*time.Millisecond), tEnd, 10*time.Millisecond)
+}
